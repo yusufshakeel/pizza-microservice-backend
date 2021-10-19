ba3bd6ad113808e1811701d5a4c539365dd5d2a5
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-10-19 16:51:42 +0530

    test

---
+++ services/payment/test/unit/src/server.unit.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fastifyModule = require('fastify');
+const Server = require('../../../src/server');
+
+describe('Server', () => {
+  describe('Setup', () => {
+    test('Should be able to setup server', async () => {
+      const fastify = fastifyModule();
+      await expect(new Server({ fastify }).setup()).resolves.not.toThrow();
+      await fastify.close();
+    });
+  });
+
+  describe('Run', () => {
+    describe('When there is no error', () => {
+      test('Should be able to run server', async () => {
+        const fastify = {
+          register: jest.fn(),
+          swagger: jest.fn(),
+          addHook: jest.fn(),
+          setErrorHandler: jest.fn(),
+          listen: jest.fn((port, host, cb) => cb())
+        };
+        const server = await new Server({ fastify }).setup();
+        await server.run();
+        expect(fastify.listen).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('When there is error', () => {
+      test('Should throw error', async () => {
+        const fastify = {
+          register: jest.fn(),
+          swagger: jest.fn(),
+          addHook: jest.fn(),
+          setErrorHandler: jest.fn(),
+          listen: jest.fn((port, host, cb) => cb('someError'))
+        };
+        try {
+          const server = await new Server({ fastify }).setup();
+          await server.run();
+        } catch (e) {
+          expect(e).toBe('someError');
+        }
+        expect(fastify.listen).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
