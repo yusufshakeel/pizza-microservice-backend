de71fb3b7166036036e26df0251bd56d239f0f07
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-10-19 12:56:48 +0530

    test

--- services/payment/test/unit/src/services/auth-token-service.unit.test.js
+++ services/payment/test/unit/src/services/auth-token-service.unit.test.js
@@ -1,20 +1,27 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
 const AuthTokenService = require('../../../../src/services/auth-token-service');
+const { JWT_SECRET } = require('../../../../src/constants');
 
 const authTokenService = new AuthTokenService();
 
-describe('createAuthToken', () => {
-  test('Should create auth token', () => {
-    const token = authTokenService.createAuthToken('87b96c89-5365-4cf0-a104-b28da006c2d7');
-    expect(token).not.toBeUndefined();
-  });
-});
-
 describe('verifyAuthToken', () => {
   describe('When token is valid', () => {
+    const validToken = userId => {
+      const now = Math.floor(Date.now() / 1000);
+      return jwt.sign(
+        {
+          userId,
+          iat: now,
+          exp: now + 10
+        },
+        JWT_SECRET
+      );
+    };
+
     test('Should not throw error', () => {
-      const token = authTokenService.createAuthToken('87b96c89-5365-4cf0-a104-b28da006c2d7');
+      const token = validToken('87b96c89-5365-4cf0-a104-b28da006c2d7');
       const result = authTokenService.verifyAuthToken(token);
       expect(result).toStrictEqual({ userId: '87b96c89-5365-4cf0-a104-b28da006c2d7' });
     });
